b30f18422ae5e48fa2a9ed513fd9bf272344bbd9
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2022-08-03 17:23:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+php index.php
 ffmpeg -i https://lightning-traceurban-samsungau.amagi.tv/playlist.m3u8 -y -vn -b:a 64k -f hls -hls_list_size 5 -hls_time 2 -hls_delete_threshold 1 -hls_flags delete_segments trace.m3u8
