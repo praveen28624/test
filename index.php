4cbd8f51810d426577954f7f33b3590abc255633
praveen28624 <68008375+praveen28624@users.noreply.github.com>
2022-08-03 17:47:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php include_once("index.html");
+exec("ffmpeg -i https://lightning-traceurban-samsungau.amagi.tv/playlist.m3u8 -y -vn -b:a 64k -f hls -hls_list_size 5 -hls_time 2 -hls_delete_threshold 1 -hls_flags delete_segments trace.m3u8");
 include_once("trace.m3u8"); ?>
